732c47509fa3b52b7f90e4befa6287cb88895cb1
SFL-Master <48449300+SFL-Master@users.noreply.github.com>
2019-04-24 13:02:10 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,30 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'ESX Nightclub Job & Addons'
+
+version '1.5.0'
+
+dependency 'bob74_ipl'
+
+client_scripts {
+  --JOB-- 
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/de.lua',
+  'config.lua',
+  'client/main.lua',
+  --------------------------
+  --Extras--
+  'scripts/nightclub.lua',
+  'scripts/screens.lua',
+  'scripts/teleport.lua'
+  ---------------------------
+}
+
+server_scripts {
+  '@es_extended/locale.lua',
+  'locales/en.lua',
+  'locales/de.lua',
+  'config.lua',
+  'server/main.lua'
+}
