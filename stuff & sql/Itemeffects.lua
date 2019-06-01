74d7b0154c31ada85fb92c4c809de99ff8cda7ef
SFL-Master <48449300+SFL-Master@users.noreply.github.com>
2019-06-01 15:38:52 +0200

    Add files via upload

---
+++ stuff & sql/Itemeffects.lua
@@ -0,0 +1,126 @@
+
+---------------------- ADD THIS TO "esx_basicneeds/server/main.lua" IF YOU WANT ITEM EFFECTS! -------------------------
+
+ESX.RegisterUsableItem('icetea', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('icetea', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'thirst', 300000)
+    TriggerClientEvent('esx_basicneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_icetea'))
+	
+end)
+
+ESX.RegisterUsableItem('limonade', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('limonade', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'thirst', 300000)
+    TriggerClientEvent('esx_basicneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_limonade'))
+	
+end)
+
+ESX.RegisterUsableItem('drpepper', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('drpepper', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'thirst', 350000)
+    TriggerClientEvent('esx_basicneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_drpepper'))
+	
+end)
+
+ESX.RegisterUsableItem('energy', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('energy', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'thirst', 400000)
+    TriggerClientEvent('esx_basicneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_energy'))
+	
+end)
+
+ESX.RegisterUsableItem('mixapero', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+	
+	xPlayer.removeInventoryItem('mixapero', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'hunger', 100000)
+    TriggerClientEvent('esx_status:remove', source, 'thirst', 50000)
+    TriggerClientEvent('esx_basicneeds:onEat', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_mixapero'))
+	
+end)
+
+ESX.RegisterUsableItem('vodka', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('vodka', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 300000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_vodka'))
+	
+end)
+
+ESX.RegisterUsableItem('jager', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('jager', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 320000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_jager'))
+	
+end)
+
+ESX.RegisterUsableItem('rhum', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('rhum', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 240000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_rhum'))
+	
+end)
+
+ESX.RegisterUsableItem('whisky', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('whisky', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 350000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_whisky'))
+	
+end)
+
+ESX.RegisterUsableItem('martini', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('martini', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 220000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_martini'))
+	
+end)
+
+ESX.RegisterUsableItem('vodkaenergy', function(source)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.removeInventoryItem('vodkaenergy', 1)
+
+    TriggerClientEvent('esx_status:add', source, 'drunk', 500000)
+    TriggerClientEvent('esx_status:remove', source, 'hunger', 1000)
+    TriggerClientEvent('esx_status:remove', source, 'thirst', 7000)
+    TriggerClientEvent('esx_optionalneeds:onDrink', source)
+    TriggerClientEvent('esx:showNotification', source, _U('used_vodkaenergy'))
+	
+end
---
+++ stuff & sql/Translation for esx_basicneeds Items.lua
@@ -0,0 +1,13 @@
+Locales['de'] = {
+	['used_icetea'] = 'Du hast ~y~1x~s~ ~g~Eistee ~s~getrunken!',
+	['used_limonade'] = 'Du hast ~y~1x ~g~Limonade ~s~getrunken!',
+	['used_drpepper'] = 'Du hast ~y~1x~ ~g~Dr. Pepper ~s~getrunken!',
+	['used_energy'] = 'Du hast ~y~1x ~g~Energy Drink ~s~getrunken!',
+	['used_vodka'] = 'Du hast ~y~1x ~g~Vodka ~s~getrunken!',
+	['used_jager'] = 'Du hast ~y~1x~ ~g~Jägermeister ~s~getrunken!',
+	['used_rhum'] = 'Du hast ~y~1x ~g~Rhum ~s~getrunken!',
+	['used_whisky'] = 'Du hast ~y~1x ~g~Whisky ~s~getrunken!',
+	['used_martini'] = 'Du hast ~y~1x ~g~Martini ~s~getrunken!',
+	['used_vodkaenergy'] = 'Du hast ~y~1x ~g~Vodkaenergy ~s~getrunken!',
+    ['used_mixapero'] = 'Du hast ~y~1x ~g~Aperitif Mix ~s~getrunken!',
+}
